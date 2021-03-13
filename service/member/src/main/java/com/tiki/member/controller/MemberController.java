1ad042c1e3f6ff1d38e9b95debae6df7847af885
DESKTOP-507JULO\manbou <tjddn0197@gmail.com>
2021-03-13 20:27:35 +0900

    컨트롤러 추가, id insert
    예시확인을 위한 삽입

--- service/member/src/main/java/com/tiki/member/controller/MemberController.java
+++ service/member/src/main/java/com/tiki/member/controller/MemberController.java
@@ -3,9 +3,7 @@ package com.tiki.member.controller;
 import com.tiki.member.domain.MemberDTO;
 import com.tiki.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MemberController {
@@ -19,4 +17,13 @@ public class MemberController {
         return memberService.selectMemberDetail(id);
     }
 
+    @PostMapping("/mbr")
+    public int insertMember(@RequestParam MemberDTO memberDTO){
+        return memberService.insertMember(memberDTO);
+    }
+
+    @DeleteMapping("/mbr/{id}")
+    public int deleteMember(@PathVariable("id") String id){
+        return memberService.deleteMember(id);
+    }
 }
---
+++ 추가정보/삽입.sql
@@ -0,0 +1,2 @@
+insert into mbr(mbr_id,mbr_pwd,mbr_nm, mbr_bday, mbr_addr, mbr_email, mbr_phone) 
+values ('id','pwd','이름','생일','주소','이메일','전화');
