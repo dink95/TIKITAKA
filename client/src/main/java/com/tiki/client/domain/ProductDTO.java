246ed1a6364cd82bb229876097ba65fd4a280426
chzhclq17 <chzhclq17@naver.com>
2021-04-16 19:46:02 +0900

    상품등록, 상세보기 페이지 추가
    상품등록, 상세보기 페이지 추가

---
+++ client/src/main/java/com/tiki/client/domain/ProductDTO.java
@@ -0,0 +1,41 @@
+package com.tiki.client.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProductDTO {
+
+    /** 제품번호(pk) */
+    private int prodNo;
+
+    /** 제품이름 */
+    private String prodNm;
+
+    /** 제품가격 */
+    private int prodPrc;
+
+    /** 카테고리번호(fk) */
+    private int catNo;
+
+    /** 판매자ID(fk) */
+    private String selId;
+
+    /** 거래방법 */
+    private String way;
+
+    /** 등록날짜 */
+    private LocalDateTime wday;
+
+    /** 상품코드 */
+    private String prodCo;
+
+    /** 조회수 */
+    private int prodView;
+
+}
