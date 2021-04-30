323f52a3b3413341cc58a8668249304307a626f3
movegun1027 <94dglee@naver.com>
2021-04-30 20:38:31 +0900

    prod sample
    - 샘플 이미지 추가
    - category 기본 insert 문 추가
    - prod sample insert 문 추가
    - prod table nego 추가 in 테이블.xlsx
    - 카테고리.xlsx 추가

--- 추가정보/삽입.sql
+++ 추가정보/삽입.sql
@@ -1,2 +1,57 @@
 insert into mbr(mbr_id,mbr_pwd,mbr_nm, mbr_bday, mbr_addr, mbr_email, mbr_phone) 
 values ('id','pwd','이름','생일','주소','이메일','전화');
+
+insert into cat(cat_nm, cat_no)
+values
+('삽니다', 0);
+
+insert into cat(cat_nm, cat_no)
+values
+('패션', 10),
+('남성의류', 11),
+('여성의류', 12),
+('아동의류', 13),
+('가방', 14),
+('신발', 15),
+('모자', 16),
+('액세서리', 17),
+('화장품', 18),
+('기타', 19);
+
+insert into cat(cat_nm, cat_no)
+values
+('디지털/가전', 20),
+('PC/노트북', 21),
+('휴대폰/태블릿', 22),
+('카메라', 23),
+('게임', 24),
+('자동차', 25),
+('기타', 26);
+
+insert into cat(cat_nm, cat_no)
+values
+('가구/인테리어', 30),
+('침실', 31),
+('주방', 32),
+('거실', 33),
+('DIY', 34),
+('서재/사무용', 35),
+('기타', 36);
+
+insert into cat(cat_nm, cat_no)
+values
+('여가활동', 40),
+('등산', 41),
+('캠핑', 42),
+('골프', 43),
+('헬스', 44),
+('공연/티켓', 45),
+('여행', 46),
+('기타', 47);
+
+insert into cat(cat_nm, cat_no)
+values
+('반려동물용품', 50);
+
+insert into cat(cat_nm, cat_no)
+values('기타', 60);
