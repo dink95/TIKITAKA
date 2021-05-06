eef41bf31e80ec68a0ade466f4ccb0359d2d04ce
DESKTOP-507JULO\manbou <tjddn0197@gmail.com>
2021-05-06 16:35:13 +0900

    신고 게시판 client & service 컨트롤러 추가
    내용 추가
    - test 추가해야함

---
+++ client/src/main/java/com/tiki/client/service/ComplainService.java
@@ -0,0 +1,43 @@
+package com.tiki.client.service;
+
+import com.tiki.client.domain.ComplainDTO;
+import com.tiki.client.domain.MemberDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service("ComplainService")
+public class ComplainService {
+
+    private WebClient webClient = WebClient.builder() //공통적으로 모든 요청에 사용되는 webclient 정보
+            .baseUrl("http://localhost:8079") //gateway url
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+
+
+    public int insertComplain(ComplainDTO complainDTO){
+        return webClient.post()
+                .uri("/comp")
+                .body(Mono.just(complainDTO), ComplainDTO.class)
+                .retrieve()
+                .bodyToMono(Integer.class) //반환정보
+                .block();
+    }
+
+    /**
+     * 신고 게시판 삭제 API
+     * @param idx : 게시판 PK
+     * @return 성공 시, 1 아닐 시 , 0
+     */
+    public int deleteComplain(int idx){
+        return webClient.delete()
+                .uri("/comp/{idx}",idx)
+                .retrieve()
+                .bodyToMono(Integer.class)
+                .block();
+    }
+
+}
